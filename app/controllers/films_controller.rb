36dd1e76d1aad640660e9562851e12608e87c217
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-03-12 16:17:18 +0000

    works kinda

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -9,17 +9,17 @@ class FilmsController < ApplicationController
 
     # Create new record
     def create
-        @film = current_user.films.build(film_params)
+        @new_film = current_user.films.build(film_params)
         api = MoviesAdapter.new
         # Check if the title was actually given before doing this?
         response = api.search(film_params[:title])
         # Make sure that there is at least one result before doing this?
-        if response.success && response
+        if response.success?
             film_result = response["results"][0]
-            @film[:official_title] = film_result["title"]
-            @film[:official_overview] = film_result["overview"]
-            @film[:poster] = film_result["poster_path"]
-            if @film.save
+            @new_film[:official_title] = film_result["title"]
+            @new_film[:official_overview] = film_result["overview"]
+            @new_film[:poster] = film_result["poster_path"]
+            if @new_film.save
                 flash[:notice] = "Film added to list."
                 redirect_to root_path
             else
@@ -28,7 +28,7 @@ class FilmsController < ApplicationController
             end
         else
             flash[:alert] = "Didn't find any results from that film. Try an alternative title?"
-            redirect_to films_path
+            redirect_to root_path
         end
     end
 
